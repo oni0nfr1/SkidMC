c9751933dc51f6721ddc9145be65e452584e0e73
oni0nfr1 <oni0nfr1@pusan.ac.kr>
2026-07-18 19:16:19 +0900

    refactor(internal/schedule): remove unused ticker

--- skid/src/client/kotlin/io/github/oni0nfr1/skid/client/SkidClient.kt
+++ skid/src/client/kotlin/io/github/oni0nfr1/skid/client/SkidClient.kt
@@ -1,7 +1,6 @@
 package io.github.oni0nfr1.skid.client
 
 import io.github.oni0nfr1.skid.client.internal.kart.KartManager
-import io.github.oni0nfr1.skid.client.internal.schedule.Ticker
 import io.github.oni0nfr1.skid.client.internal.tachometer.TachometerManager
 import net.fabricmc.api.ClientModInitializer
 import org.slf4j.Logger
@@ -14,10 +13,9 @@ class SkidClient : ClientModInitializer {
         val LOGGER: Logger = LoggerFactory.getLogger(SkidClient::class.java)
     }
 
-    /** 카트, 타코미터 및 클라이언트 틱 추적기를 초기화합니다. */
+    /** 카트와 타코미터 추적기를 초기화합니다. */
     override fun onInitializeClient() {
         KartManager.init()
         TachometerManager.init()
-        Ticker.init()
     }
 }
